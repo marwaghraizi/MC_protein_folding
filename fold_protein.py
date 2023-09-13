3a238292e96eda3b8438668579501382fe2e7d52
Marwa Ghraizi <marwaghraizi@gmail.com>
2023-09-13 20:25:03 +0200

    results file generation

--- fold_protein.py
+++ fold_protein.py
@@ -88,6 +88,8 @@ if __name__ == '__main__':
     # if the input is a fasta file, read the sequence and transform it to HP if
     # needed
     if args.file:
+        file_name = args.file.rsplit(".")[0]
+        file_name = file_name.rsplit("/")[1]
         with open(args.file, "r") as filein:
             read_sequence = ""
             for line in filein:
@@ -112,7 +114,11 @@ if __name__ == '__main__':
     # if the initial conformation desired is random
     if initial_conformation == 'random':
         protein = randomize_protein(initial_linear_protein)
-        protein.graph_show("results/initial_randomized_protein")
+        if args.file:
+            protein.graph_show(f"results/{file_name}_initial_randomized_protein")
+        else:
+            protein.graph_show(
+                f"results/protein_initial_randomized_protein")
         starting_energy = protein.calculate_energy()
         print(f"Initial conformation of the protein is randomized with a "
               f"starting energy of {starting_energy}.\nFind the graph "
@@ -162,7 +168,12 @@ if __name__ == '__main__':
             print(f"Graph representation is saved to "
                   f"protein_optimal_display.png")
 
-    with open("results/protein_folding_log.txt", "w") as filout:
+    if args.file:
+        output_file = f"results/{file_name}_protein_folding_log.txt"
+    else:
+        output_file = f"results/protein_folding_log.txt"
+
+    with open(output_file, "w") as filout:
         filout.write(f"The HP model of the protein is: {sequence}\n")
         filout.write(f"The starting energy is {protein.calculate_energy()}\n")
         filout.write(f"The final energy is {final_frame.calculate_energy()}\n")
@@ -175,87 +186,3 @@ if __name__ == '__main__':
                   f"- Energy: {energy} -------", file=filout)
             print(conformation.show(), file=filout)
         filout.close()
-
-
-
-
-
-
-
-
-
-
-    # test cases from paper:
-    ## HPHPPHHPHPPHPHHPPHPH
-    ## HHPPHPPHPPHPPHPPHPPHPPHH
-    ## PPHPPHHPPPPHHPPPPHHPPPPHH
-    ## PPPHHPPHHPPPPPHHHHHHHPPHHPPPPHHPPHPP
-    ## PPHPPHHPPHHPPPPPHHHHHHHHHHPPPPPPPPHHPPHHHPPHHHHH
-
-    # throw error if amino acid sequence contains invalid amino acid symbols (example: X)
-    # throw error if n-iterations less than 1
-    # throw error if fasta file in wrong format
-    # throw error if sequence length < 2
-    # input log file name to be created
-
-    # initial_protein = Protein(ALL_RESIDUES)
-
-    # testing corner
-    # initial_protein.all_residues[0].coordI = 1
-    # initial_protein.all_residues[0].coordJ = 1
-
-    # testing crankshaft
-    # initial_protein.all_residues[1].coordI = 0
-    # initial_protein.all_residues[1].coordJ = -1
-    # initial_protein.all_residues[2].coordI = 1
-    # initial_protein.all_residues[2].coordJ = -1
-    # initial_protein.all_residues[3].coordI = 1
-    # initial_protein.all_residues[3].coordJ = 0
-
-    # Figure 3 (a) testing that pull can do the corner move
-    #     ALL_RESIDUES[0].coordJ = 3
-    #     ALL_RESIDUES[1].coordI = 0
-    #     ALL_RESIDUES[1].coordJ = 2
-    #     ALL_RESIDUES[2].coordI = 0
-    #     ALL_RESIDUES[2].coordJ = 1
-    #     ALL_RESIDUES[3].coordI = 1
-    #     ALL_RESIDUES[3].coordJ = 1
-
-    # Figure 3 (b)
-    #     ALL_RESIDUES[0].coordJ = 1
-    #     ALL_RESIDUES[1].coordI = 0
-    #     ALL_RESIDUES[2].coordI = 0
-    #     ALL_RESIDUES[3].coordI = 2
-    #     ALL_RESIDUES[2].coordI = 1
-
-    # Figure 3 (c)
-    # FOR THIS ONE, CHANGE test.fasta to 'AEAEAEAEA' (9 residue)
-
-    # ALL_RESIDUES[0].coordI = 3
-    # ALL_RESIDUES[1].coordI = 2
-    # ALL_RESIDUES[2].coordI = 1
-    # ALL_RESIDUES[3].coordI = 1
-    # ALL_RESIDUES[3].coordJ = 1
-    # ALL_RESIDUES[4].coordI = 0
-    # ALL_RESIDUES[4].coordJ = 1
-    # ALL_RESIDUES[5].coordI = 0
-    # ALL_RESIDUES[5].coordJ = 2
-    # ALL_RESIDUES[6].coordI = 1
-    # ALL_RESIDUES[6].coordJ = 2
-    # ALL_RESIDUES[7].coordI = 2
-    # ALL_RESIDUES[7].coordJ = 2
-    # ALL_RESIDUES[8].coordI = 3
-    # ALL_RESIDUES[8].coordJ = 2
-
-    # print("INITIAL PROTEIN COORDINATES:")
-    # print(initial_protein.coordinates)
-    # print()
-    # manipulation = Manipulation()
-    # manipulation.add_frame(initial_protein, 'initial')
-    # manipulation.apply_monte_carlo()
-    # # figure b
-    # # manipulation.pull_move(manipulation.all_frames[0].copy_protein(), initial_protein.all_residues[2])
-    # # figure 3 c
-    # # manipulation.pull_move(manipulation.all_frames[0].copy_protein(), initial_protein.all_residues[7])
-    #
-    # manipulation.show_all_frames()
